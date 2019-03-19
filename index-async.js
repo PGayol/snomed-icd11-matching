0693a976a1cdaa98f3c7a1aa1fc28d503b55e154
Patricio Gayol <patricio@termmed.com>
2019-03-19 20:15:39 -0300

    Added valid token check in search request

--- index-async.js
+++ index-async.js
@@ -13,8 +13,10 @@ let access_token;
 
 
 //XLS source details
-var sourceWorkbook = XLSX.readFile('mini-snomed-list.xls');
-var sourceWorksheet = sourceWorkbook.Sheets['Hoja1'];
+let sourceWorkbook = XLSX.readFile('mini-snomed-list.xls');
+let sourceWorksheet = sourceWorkbook.Sheets['Hoja1'];
+let searchIndex = 2; //if data matrix starts in row 2
+let finalIndex = 6194 + 2; // #ConceptsToSearch + 2
 
 //Options object to request token
 let options = {
@@ -27,104 +29,104 @@ let options = {
   }
 };
 
-request.post(options, (error, res, body) => {
-  if (error) {
-    console.error(error)
-    return
-  }
-  if (`${res.statusCode}` == 200) {
-    console.log("Successfully connected to server");
-  }
-  console.log(`statusCode: ${res.statusCode}`);
-  var info = JSON.parse(body)
-  access_token = info.access_token;
+sourceWorksheet['A1'] = sourceWorksheet['A1'] ? sourceWorksheet['A1'] : {};
+sourceWorksheet['A1'].t = 's';
+sourceWorksheet['A1'].v = 'Concept ID'
+sourceWorksheet['B1'] = sourceWorksheet['B1'] ? sourceWorksheet['B1'] : {};
+sourceWorksheet['B1'].t = 's';
+sourceWorksheet['B1'].v = 'Name'
+sourceWorksheet['C1'] = sourceWorksheet['C1'] ? sourceWorksheet['C1'] : {};
+sourceWorksheet['C1'].t = 's';
+sourceWorksheet['C1'].v = 'Effective Time'
+sourceWorksheet['D1'] = sourceWorksheet['D1'] ? sourceWorksheet['D1'] : {};
+sourceWorksheet['D1'].t = 's';
+sourceWorksheet['D1'].v = 'ICD11 match'
+sourceWorksheet['E1'] = sourceWorksheet['E1'] ? sourceWorksheet['E1'] : {};
+sourceWorksheet['E1'].t = 's';
+sourceWorksheet['E1'].v = 'Score'
 
-  //Options object to request search
-  searchOptions = {
-    uri: 'https://id.who.int/icd/entity/search?q={}',
-    headers: {
-      'Authorization': 'Bearer ' + access_token,
-      'Accept': 'application/json',
-      'Accept-Language': 'en'
+let main = function () {
+
+  request.post(options, (error, res, body) => {
+    if (error) {
+      console.error(error)
+      return
     }
-  }
+    if (`${res.statusCode}` == 200) {
+      console.log("Successfully connected to server");
+    }
+    console.log(`statusCode: ${res.statusCode}`);
+    let info = JSON.parse(body)
+    access_token = info.access_token;
 
-  let searchIndex = 2; //if data matrix starts in row 2
-  let finalIndex = 6194 + 2; // #ConceptsToSearch + 2
-  sourceWorksheet['A1'] = sourceWorksheet['A1'] ? sourceWorksheet['A1'] : {};
-  sourceWorksheet['A1'].t = 's';
-  sourceWorksheet['A1'].v = 'Concept ID'
-  sourceWorksheet['B1'] = sourceWorksheet['B1'] ? sourceWorksheet['B1'] : {};
-  sourceWorksheet['B1'].t = 's';
-  sourceWorksheet['B1'].v = 'Name'
-  sourceWorksheet['C1'] = sourceWorksheet['C1'] ? sourceWorksheet['C1'] : {};
-  sourceWorksheet['C1'].t = 's';
-  sourceWorksheet['C1'].v = 'Effective Time'
-  sourceWorksheet['D1'] = sourceWorksheet['D1'] ? sourceWorksheet['D1'] : {};
-  sourceWorksheet['D1'].t = 's';
-  sourceWorksheet['D1'].v = 'ICD11 match'
-  sourceWorksheet['E1'] = sourceWorksheet['E1'] ? sourceWorksheet['E1'] : {};
-  sourceWorksheet['E1'].t = 's';
-  sourceWorksheet['E1'].v = 'Score'
-  
+    //Options object to request search
+    searchOptions = {
+      uri: 'https://id.who.int/icd/entity/search?q={}',
+      headers: {
+        'Authorization': 'Bearer ' + access_token,
+        'Accept': 'application/json',
+        'Accept-Language': 'en'
+      }
+    }
 
-  var getRequest = function (searchIndex) {
-    if (searchIndex == finalIndex) {
-      return;
-    } else {
-      let sourceCell = 'B' + searchIndex;
-      let resultCell = 'D' + searchIndex;
-      let scoreCell = 'E' + searchIndex;
-      const range = {
-        s: {
-          c: 0,
-          r: 0
-        },
-        e: {
-          c: 100,
-          r: searchIndex + 1
-        }
-      };
-      sourceWorksheet['!ref'] = XLSX.utils.encode_range(range);
-      searchOptions.uri = 'https://id.who.int/icd/entity/search?q={' + sourceWorksheet[sourceCell].v + '}';
-      sourceWorksheet[resultCell] = sourceWorksheet[resultCell] ? sourceWorksheet[resultCell] : {};
-      sourceWorksheet[resultCell].t = 's';
-      sourceWorksheet[scoreCell] = sourceWorksheet[scoreCell] ? sourceWorksheet[scoreCell] : {};
-      sourceWorksheet[scoreCell].t = 'n';
-      request.get(searchOptions, (error, res, body) => {
-        if (error) {
-          console.error(error);
-          sourceWorksheet[resultCell].v = 'Bad request';
-          return;
-        }
-        console.log('Cell' + sourceCell + ' request returned ' + `statusCode: ${res.statusCode}`);
-        let info = JSON.parse(body);
-        info.DestinationEntities.sort(function (a, b) {
-          return b.Score - a.Score;
+    let getRequest = function (searchIndex) {
+      if (searchIndex == finalIndex) {
+        return;
+      } else {
+        let sourceCell = 'B' + searchIndex;
+        let resultCell = 'D' + searchIndex;
+        let scoreCell = 'E' + searchIndex;
+        const range = {
+          s: {
+            c: 0,
+            r: 0
+          },
+          e: {
+            c: 100,
+            r: searchIndex + 1
+          }
+        };
+        sourceWorksheet['!ref'] = XLSX.utils.encode_range(range);
+        searchOptions.uri = 'https://id.who.int/icd/entity/search?q={' + sourceWorksheet[sourceCell].v + '}';
+        sourceWorksheet[resultCell] = sourceWorksheet[resultCell] ? sourceWorksheet[resultCell] : {};
+        sourceWorksheet[resultCell].t = 's';
+        sourceWorksheet[scoreCell] = sourceWorksheet[scoreCell] ? sourceWorksheet[scoreCell] : {};
+        sourceWorksheet[scoreCell].t = 'n';
+        request.get(searchOptions, (error, res, body) => {
+          if (res.statusCode == 401 || res.statusCode == '401') {
+            main();
+          } else {
+            if (error) {
+              console.error(error);
+              sourceWorksheet[resultCell].v = 'Bad request';
+              return;
+            }
+            console.log('Cell' + sourceCell + ' request returned ' + `statusCode: ${res.statusCode}`);
+            let info = JSON.parse(body);
+            info.DestinationEntities.sort(function (a, b) {
+              return b.Score - a.Score;
+            });
+            if (info.DestinationEntities.length == 0) {
+              sourceWorksheet[resultCell].v = 'No match';
+            } else {
+              sourceWorksheet[resultCell].v = info.DestinationEntities[0].Title;
+              sourceWorksheet[scoreCell].v = info.DestinationEntities[0].Score;
+            }
+            XLSX.writeFile(sourceWorkbook, 'mini-snomed-list-results.xls');
+
+            // console.log("Best match for SNOMEDCT term:'" + worksheet[cell].v + "' is:" + info.DestinationEntities[0].Title);
+            // let i = 0;
+            // while (i < info.DestinationEntities.length) {
+            //   console.log("Best match:" + info.DestinationEntities[i].Title + ' with a score of: ' + info.DestinationEntities[i].Score);
+            //   i++;
+            // }
+            searchIndex++;
+            getRequest(searchIndex);
+          }
         });
-        if (info.DestinationEntities.length == 0) {
-          sourceWorksheet[resultCell].v = 'No match';
-        } else {
-          sourceWorksheet[resultCell].v = info.DestinationEntities[0].Title;
-          sourceWorksheet[scoreCell].v = info.DestinationEntities[0].Score;
-        }
-        XLSX.writeFile(sourceWorkbook, 'mini-snomed-list-results.xls');
-
-        // console.log("Best match for SNOMEDCT term:'" + worksheet[cell].v + "' is:" + info.DestinationEntities[0].Title);
-        // let i = 0;
-        // while (i < info.DestinationEntities.length) {
-        //   console.log("Best match:" + info.DestinationEntities[i].Title + ' with a score of: ' + info.DestinationEntities[i].Score);
-        //   i++;
-        // }
-        searchIndex++;
-        getRequest(searchIndex);
-
-      });
-
-
-    };
-
-  }
-  getRequest(searchIndex);
-
-});
+      };
+    }
+    getRequest(searchIndex);
+  });
+}
+main();
